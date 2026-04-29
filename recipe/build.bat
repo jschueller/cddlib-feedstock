e2dba8a8fce5a42f685da8f7f2121f2beeed9b87
Julien Schueller <schueller@phimeca.com>
2026-04-29 11:51:19 +0200

    Enable windows build

---
+++ recipe/build.bat
@@ -0,0 +1,12 @@
+copy %RECIPE_DIR%\CMakeLists.txt .
+copy %RECIPE_DIR%\FindGMP.cmake .
+
+cmake -LAH -G "Ninja" ^
+    -DCMAKE_PREFIX_PATH="%LIBRARY_PREFIX%" ^
+    -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%" ^
+    -B build .
+if errorlevel 1 exit 1
+
+cmake --build build --target install --config Release --parallel %CPU_COUNT%
+if errorlevel 1 exit 1
+
